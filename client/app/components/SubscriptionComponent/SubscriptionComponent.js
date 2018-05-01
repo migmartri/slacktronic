59a3a33388037cb05847736a684ad097da345212
Miguel Martinez <miguel@bitnami.com>
2018-04-30 23:02:44 -0700

    subscription page

--- client/app/components/SubscriptionComponent/SubscriptionComponent.js
+++ client/app/components/SubscriptionComponent/SubscriptionComponent.js
@@ -1,8 +1,9 @@
 // @flow
 import React, { Component } from 'react';
-import { Badge, Card, Tag } from 'antd';
+import { Badge, Card, Tag, Icon, Row, Col } from 'antd';
 import type { subscriptionType } from '../../models/subscription';
 import SlacktronicSerialClient from '../../lib/serialClient';
+import styles from './subscription.scss';
 
 type Props = {
   subscription: subscriptionType,
@@ -17,12 +18,22 @@ export default class SubscriptionComponent extends Component<Props> {
     const sub = this.props.subscription;
     return (
       <div>
-        <Card title={sub.assertion.name}>
+        <Card>
           <p>{sub.assertion.description}</p>
-          <Tag>Slot: {sub.slot}</Tag>
-          <Badge count={sub.active ? 1 : 0}>
-            <Tag color={sub.active ? 'green' : 'blue'}>Status: {sub.active ? 'ON' : 'OFF'}</Tag>
-          </Badge>
+          <Row className={styles.icons}>
+            <Col span={8}><Icon type="slack" /></Col>
+            <Col span={8}><Icon type="arrow-right" /></Col>
+            <Col span={8}><Icon type="usb" /></Col>
+          </Row>
+          <ul className={styles.summary}>
+            <li>Trigger: {sub.assertion.name}</li>
+            <li>Action: Serial message {sub.slot}</li>
+            <li>Status:
+              <Badge count={sub.active ? 1 : 0}>
+                <Tag color={sub.active ? 'green' : 'blue'}>{sub.active ? 'ON' : 'OFF'}</Tag>
+              </Badge>
+            </li>
+          </ul>
         </Card>
       </div>
     );
