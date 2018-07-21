8f1553c19afd4e825a2b67dd826574d3f9ddcf0b
Miguel Martinez <migmartri@gmail.com>
2018-07-20 17:53:34 -0700

    Manually trigger

--- client/app/containers/SubscriptionContainer/SubscriptionContainer.js
+++ client/app/containers/SubscriptionContainer/SubscriptionContainer.js
@@ -2,8 +2,10 @@
 import { connect } from 'react-redux';
 import type { Dispatch } from '../../actions/common';
 import type { subscriptionType } from '../../models/subscription';
+import type { triggerType } from '../../models/trigger';
 import SubscriptionComponent from '../../components/SubscriptionComponent';
 import { deleteSubscription } from '../../actions/subscriptions';
+import { triggered } from '../../actions/triggers';
 
 const mapStateToProps = (state, ownProps) => {
   const providersByName = state.providers.byName;
@@ -21,6 +23,9 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   onDelete: (subscription: subscriptionType) => {
     dispatch(deleteSubscription(subscription));
+  },
+  onManualTrigger: (trigger: triggerType, checked: boolean) => {
+    dispatch(triggered(trigger.ID, checked, 0));
   }
 });
 
